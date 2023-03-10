b0fadd656f01e9a7e5afa841c8d2e4b8f7fb1051
Darren <58746026+Dazz563@users.noreply.github.com>
2023-03-10 17:22:56 +0200

    updates to image editing and menu

--- src/app/admin/user-model/user-model.component.ts
+++ src/app/admin/user-model/user-model.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef, MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {HelperService} from 'src/app/services/helper.service';
+import {environment} from 'src/environments/environment';
 
 @Component({
 	selector: 'app-user-model',
@@ -8,14 +10,19 @@ import {MAT_DIALOG_DATA, MatDialogRef, MatDialog, MatDialogConfig} from '@angula
 	styleUrls: ['./user-model.component.scss'],
 })
 export class UserModelComponent implements OnInit {
+	apiUrl = environment.apiUrl;
 	deleteModal = false;
 	actionType: string;
+	viewImage: string;
+	updateImage: string = null;
+	file: any;
 
 	userForm: FormGroup;
 
 	constructor(
 		@Inject(MAT_DIALOG_DATA) public data, //
-		private modalRef: MatDialogRef<UserModelComponent>
+		private modalRef: MatDialogRef<UserModelComponent>,
+		private helper: HelperService
 	) {}
 
 	ngOnInit(): void {
@@ -35,11 +42,13 @@ export class UserModelComponent implements OnInit {
 			);
 		} else if (this.data.action === 'update') {
 			console.log(this.data);
+			this.viewImage = this.apiUrl + this.data.avatar;
 
 			this.userForm = new FormGroup({
 				id: new FormControl('', [Validators.required]),
 				name: new FormControl('', [Validators.required]),
 				email: new FormControl('', [Validators.required, Validators.email]),
+				avatar: new FormControl(''),
 				admin: new FormControl(false),
 				vendor: new FormControl(false),
 				user: new FormControl(true),
@@ -67,7 +76,52 @@ export class UserModelComponent implements OnInit {
 	}
 
 	submit() {
-		this.modalRef.close(this.userForm.value);
+		/**
+		 * Check if the image was updated & remove path laravel sent to display image
+		 */
+		if (!this.updateImage) {
+			this.userForm.patchValue({
+				'avatar': this.removeAvatarPath(this.userForm.value.avatar),
+			});
+			console.log(this.userForm.value);
+
+			this.modalRef.close(this.userForm.value);
+			return;
+		}
+		/**
+		 * Or we submit image to api and use response to ref image
+		 */
+		this.helper.uploadImageWebCompress(['/api/upload_avatar', this.file]).subscribe({
+			next: (res: any) => {
+				if (res.data) {
+					console.log(res);
+					this.userForm.patchValue({
+						'avatar': res.data,
+					});
+					// Save users
+					console.log(this.userForm.value);
+					this.modalRef.close(this.userForm.value);
+				}
+			},
+			error: (error) => {
+				console.log(error);
+			},
+		});
+	}
+
+	updateAvatar($event: Event) {
+		this.file = ($event.target as HTMLInputElement).files[0];
+		const reader = new FileReader();
+		reader.onload = () => {
+			this.viewImage = null;
+			this.updateImage = reader.result as string;
+			// this.imageSelected = true; // update the property when an image is selected
+		};
+		reader.readAsDataURL(this.file);
+	}
+
+	removeAvatarPath(str) {
+		return str.replace('/storage/avatars/', '');
 	}
 
 	onDelete() {
